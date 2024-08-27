08694d40d6ad179911295577770dd3a4a8707b42
Alex Marolleau <Alex.M79@outlook.com>
2024-08-27 15:31:07 +0200

    About OK

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import '../styles/About.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJs, faReact, faNodeJs, faPython, faDocker } from '@fortawesome/free-brands-svg-icons';
+import { faPaintBrush, faServer, faCode } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState([]);
+  const [isVisible1, setIsVisible1] = useState(false);
+  const [isVisible2, setIsVisible2] = useState(false);
+  const terminal1Ref = useRef(null);
+  const terminal2Ref = useRef(null);
+
+  const fullText1 = "Bonjour ! Moi c'est Alex Marolleau, développeur web passionné par la création d'applications web dynamiques et engageantes. J'ai suivi plusieurs formations dans le web et obtenu un Bac +2 en développement web, que j'ai réalisé en alternance. Je préfère le développement front-end, mais j'ai également une solide expérience dans la création d'applications full-stack.";
+
+  // Utilisation de useMemo pour mémoriser le tableau
+  const fullText2 = useMemo(() => [
+    { text: <strong>Maîtrisé avec:</strong>, icon: null },
+    { text: 'HTML', icon: faHtml5 },
+    { text: 'CSS', icon: faCss3Alt },
+    { text: 'JavaScript', icon: faJs },
+    { text: 'React', icon: faReact },
+    { text: 'Node.js', icon: faNodeJs },
+    { text: 'Express.js', icon: faServer },
+    { text: <><br /><strong>Exposé à:</strong></>, icon: null },
+    { text: 'Python', icon: faPython },
+    { text: 'Adobe Illustrator', icon: faPaintBrush },
+    { text: <><br /><strong>Actuellement en apprentissage:</strong></>, icon: null },
+    { text: 'TypeScript', icon: faCode },
+    { text: 'Docker', icon: faDocker },
+    { text: 'Next.js', icon: faReact },
+  ], []);
+
+  const createObserver = useCallback((setIsVisible) => {
+    return new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+        }
+      },
+      { threshold: 0.65 }
+    );
+  }, []);
+
+  useEffect(() => {
+    const observer1 = createObserver(setIsVisible1);
+    const observer2 = createObserver(setIsVisible2);
+    const current1 = terminal1Ref.current;
+    const current2 = terminal2Ref.current;
+
+    if (current1) observer1.observe(current1);
+    if (current2) observer2.observe(current2);
+
+    return () => {
+      if (current1) observer1.unobserve(current1);
+      if (current2) observer2.unobserve(current2);
+    };
+  }, [createObserver]);
+
+  const animateText = useCallback((fullText, setText, isArray = false) => {
+    let index = 0;
+    const typingSpeed = 10;
+
+    const timer = setInterval(() => {
+      if (isArray) {
+        setText(fullText.slice(0, index + 1));
+      } else {
+        setText(fullText.slice(0, index));
+      }
+      index++;
+      if (index > fullText.length) clearInterval(timer);
+    }, typingSpeed);
+
+    return timer;
+  }, []);
+
+  useEffect(() => {
+    if (isVisible1) {
+      const timer1 = animateText(fullText1, setText1);
+      return () => clearInterval(timer1);
+    }
+  }, [isVisible1, fullText1, animateText]);
+
+  useEffect(() => {
+    if (isVisible2) {
+      const timer2 = animateText(fullText2, setText2, true);
+      return () => clearInterval(timer2);
+    }
+  }, [isVisible2, fullText2, animateText]);
+
   return (
     <section id="about">
-      <div className="terminal">
+      <div className="terminal" ref={terminal1Ref}>
         <div className="terminal-header">
           <div className="button red"></div>
           <div className="button yellow"></div>
           <div className="button green"></div>
         </div>
         <div className="terminal-body">
-          <p className="command">alexmarolleau $ cat aboutalex</p>
-          <p className="response">aboutalex (main) $ Bonjour ! Je suis Alex Marolleau, développeur web passionné par la création d'applications web dynamiques et engageantes. J'ai suivi une formation en webdesign et obtenu un Bac +2 en développement web, que j'ai réalisé en alternance.</p>
+          <p className="command">alexmarolleau $ cd aboutalex</p>
+          <p className="response"><span className="typing-text">{text1}</span><span className="cursor">|</span></p>
         </div>
       </div>
-      <div className="terminal">
+      <div className="terminal" ref={terminal2Ref}>
         <div className="terminal-header">
           <div className="button red"></div>
           <div className="button yellow"></div>
@@ -23,22 +110,14 @@ const About = () => {
         </div>
         <div className="terminal-body">
           <p className="command">alexmarolleau $ cd compétences/outils</p>
-          <p className="command">compétences/outils (main) $ ls</p>
-          <p className="response">Maîtrisé avec</p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>PHP</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>Express.js</li>
-          </ul>
-          <p className="response">Exposé à</p>
-          <ul>
-            <li>Python</li>
-            <li>Adobe Illustrator</li>
-          </ul>
+          <div className="typing-text">
+            {text2.map((item, index) => (
+              <p key={index}>
+                {item.text} {item.icon && <FontAwesomeIcon icon={item.icon} />}
+              </p>
+            ))}
+            <span className="cursor">|</span>
+          </div>
         </div>
       </div>
     </section>
