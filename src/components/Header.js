c2786d511e889235ac2bafc1a80290f7dc2cc739
Alex Marolleau <Alex.M79@outlook.com>
2024-08-27 15:28:58 +0200

    Header OK

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Header.scss';
 
-const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+const Header = ({ isDarkMode, onToggleDarkMode }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    document.body.classList.toggle('dark-mode', !isDarkMode);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    document.body.classList.toggle('menu-open', !isMenuOpen);
   };
+
+  useEffect(() => {
+    return () => {
+      document.body.classList.remove('menu-open');
+    };
+  }, []);
 
   return (
     <header>
-      <nav>
-        <ul>
-          <li><Link to="home" smooth={true} duration={500}>home</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>about me</Link></li>
+      <nav className={isMenuOpen ? 'open' : ''}>
+        <span className="menu-toggle" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </span>
+        <ul className={isMenuOpen ? 'open' : ''}>
+          <li><Link to="home" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>home</Link></li>
+          <li><Link to="about" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>about me</Link></li>
           <h1>am</h1>
-          <li><Link to="projects" smooth={true} duration={500}>portfolio</Link></li>
-          <li><Link to="contact" smooth={true} duration={500}>contact</Link></li>
+          <li><Link to="projects" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>portfolio</Link></li>
+          <li><Link to="contact" smooth={true} duration={500} onClick={() => setIsMenuOpen(false)}>contact</Link></li>
           <li>
-            <button onClick={toggleDarkMode} className="dark-mode-toggle">
+            <span onClick={onToggleDarkMode} className="dark-mode-toggle">
               <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
-            </button>
+            </span>
           </li>
         </ul>
       </nav>
